7357e8614ac24a240901d4653bea934015278b7b
Greg James <gregvjames99@gmail.com>
2026-01-17 20:59:38 -0600

    Read file header

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::Read;
 use anyhow::bail;
+use std::io::Cursor;
 
 enum CompressionLevel {
     Low,
@@ -8,10 +10,22 @@ enum CompressionLevel {
 // https://www.w3.org/TR/png-3/#4Concepts.Encoding
 const PNG_SIG: [u8; 8] = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 
-fn main() {
-    let file = match std::fs::File::open("SailFlow.png") {
+fn main() -> anyhow::Result<()> {
+    let mut file = match std::fs::File::open("SailFlow.png") {
         Ok(file) => file,
         Err(e) => panic!("Failed to open file: {}", e),
     };
 
+    let mut bytes: Vec<u8> = Vec::new();
+    file.read_to_end(&mut bytes).unwrap();
+
+    let mut cursor = Cursor::new(bytes);
+    let mut signature: [u8; 8] = [0u8; 8];
+    cursor.read_exact(&mut signature).map_err(|e| e.to_string()).unwrap();
+
+    if signature != PNG_SIG {
+        bail!("Signature doesn't match PNG signature");
+    }
+
+    Ok(())
 }
