7a0be480e1fc224d8f7057dfe29b2694f7a8f156
Greg James <gregvjames99@gmail.com>
2026-01-17 22:45:57 -0600

    Add paeth_predictor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 use anyhow::bail;
 use std::io::Cursor;
+use std::simd::u8x1;
 use byteorder::{BigEndian, ReadBytesExt};
 use flate2::read::ZlibDecoder;
 
@@ -178,6 +179,26 @@ fn main() -> anyhow::Result<()> {
         unfilter_row(filter_type, bytes_per_pixel, source, prev, dest);
     }
 
+    fn paeth_predictor(a: u8, b: u8, c: u8) -> u8{
+        //convert to i32 since we may need negatives here for abs
+        let a = a as i32;
+        let b = b as i32;
+        let c = c as i32;
+
+        let p = a + b - c;
+        let pa = (p - a).abs();
+        let pb = (p - b).abs();
+        let pc = (p - c).abs();
+
+        if pa <= pb && pa <= pc {
+            a as u8
+        } else if pb <= pc {
+            b as u8
+        }
+        else {
+            c as u8
+        }
+    }
     fn unfilter_row(filter_type: u8, bytes_per_pixel: usize, src: &[u8], prev: Option<Vec<u8>>, dst: &mut [u8]){
         let prev = prev.unwrap_or(vec![0u8; src.len()]);
 
@@ -199,7 +220,12 @@ fn main() -> anyhow::Result<()> {
             },
             3 => { // Avg
                 // Recon(x) = Filt(x) + floor((Recon(a) + Recon(b)) / 2)
-                
+                for i in 0..src.len() {
+                    let left = if i >= bytes_per_pixel { dst[i - bytes_per_pixel] } else { 0 };
+                    let up = prev[i];
+                    let avg = ((left as u16 + up as u16) / 2) as u8;
+                    dst[i] = src[i].wrapping_add(avg);
+                }
             },
             4 => { // Paeth
 
